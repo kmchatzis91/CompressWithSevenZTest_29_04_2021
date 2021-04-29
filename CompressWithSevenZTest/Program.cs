b020ccb1458ed64c0389945045945abb196a7e2b
Konstantinos Chatzis <67655091+kmchatzis91@users.noreply.github.com>
2021-04-29 16:21:18 +0300

    Add project files.

---
+++ CompressWithSevenZTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SevenZipNET;
+using System.IO;
+
+namespace CompressWithSevenZTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Variables
+            string sourceDirectory = "Source_Directory";
+            string destinationDirectory = "Destination_Directory";
+            #endregion
+
+            SevenZHandle.InitSevenZHandle(sourceDirectory, destinationDirectory);
+
+        } // END static void Main(string[] args)
+
+    } // END class Program
+
+} // END namespace CompressWithSevenZTest
---
+++ CompressWithSevenZTest/SevenZHandle.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SevenZipNET;
+
+namespace CompressWithSevenZTest
+{
+	class SevenZHandle
+	{
+        public static void InitSevenZHandle(string sourceDirectory, string destinationDirectory) 
+        {
+            #region Variables
+            string date = DateTime.UtcNow.ToString("yyyy_MM_dd_HH_mm_ss");
+            string sevenZipName = "7z_Result_" + date;           
+            string sevenZDirectory = Path.Combine(destinationDirectory, sevenZipName);
+            var allCsvLstBinFiles = Directory.EnumerateFiles(sourceDirectory)
+                .Where(file => file.ToLower().EndsWith("csv") || file.ToLower().EndsWith("lst") || file.ToLower().EndsWith("bin"))
+                .ToList();
+            #endregion
+
+            CheckIfDirectoryExistsElseCreate(sevenZDirectory);
+            CreateDirectoryWithSelectedFiles(sourceDirectory, sevenZDirectory, allCsvLstBinFiles);
+            CompressGivenDirectory(sevenZDirectory);
+            KeepOnlyCompressedDirectory(sevenZDirectory);
+
+        } // END public static void InitSevenZHandle()
+
+        private static void KeepOnlyCompressedDirectory(string sevenZDirectory)
+        {
+            Directory.Delete(sevenZDirectory, true);
+
+        } // END private static void KeepOnlyCompressedDirectory(string sevenZDirectory)
+
+        private static void CompressGivenDirectory(string sevenZDirectory)
+        {
+            SevenZipCompressor sevenZipCompressor = new SevenZipCompressor(sevenZDirectory);
+            sevenZipCompressor.CompressDirectory(sevenZDirectory, CompressionLevel.Normal);
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("\n All Files are compressed! \n");
+            Console.ForegroundColor = ConsoleColor.White;
+
+        } // END private static void CompressGivenDirectory(string sevenZDirectory)
+
+        private static void CreateDirectoryWithSelectedFiles(string sourceDirectory, string sevenZDirectory, IEnumerable<string> allCsvLstBinFiles)
+        {
+            foreach (var csvLstBinFile in allCsvLstBinFiles)
+            {
+                FileInfo infoCsvLstBinFile = new FileInfo(csvLstBinFile);
+                string file = Path.Combine(sourceDirectory, infoCsvLstBinFile.Name);
+                string movedFile = Path.Combine(sevenZDirectory, infoCsvLstBinFile.Name);
+                File.Move(file, movedFile);
+            }
+
+        } // END private static void CreateDirectoryWithSelectedFiles(string sourceDirectory, string sevenZDirectory, IEnumerable<string> allCsvLstFiles)
+
+        private static void CheckIfDirectoryExistsElseCreate(string sevenZDirectory)
+        {
+            bool directoryExists = Directory.Exists(sevenZDirectory);
+
+            if (!directoryExists)
+            {
+                Directory.CreateDirectory(sevenZDirectory);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\n Directory ' {0} ' created!", sevenZDirectory);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+        } // END private static void CheckIfDirectoryExistsElseCreate(string sevenZDirectory)
+
+    } // END class SevenZHandle
+
+} // END namespace CompressWithSevenZTest
